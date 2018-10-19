bd11d3555b5054eb4b7b7b4fbd829108d26d5c8a
carlwang100 <279311230@qq.com>
2018-10-19 18:17:40 +0800

    使用xfermode绘制圆角图

--- app/src/main/java/widget/RoundImageActivity.java
+++ app/src/main/java/widget/RoundImageActivity.java
@@ -8,6 +8,9 @@ import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.os.Bundle;
@@ -20,7 +23,7 @@ import com.example.wangchao.testproject.activity.BaseActivity;
 
 public class RoundImageActivity extends BaseActivity {
 
-    private ImageView mShaderImg;
+    private ImageView mShaderImg, mXferomodeImg;
 
     public static void startActivity(Context context) {
         Intent i = new Intent(context, RoundImageActivity.class);
@@ -46,6 +49,7 @@ public class RoundImageActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mShaderImg = findViewById(R.id.bitmap_shader_image);
+        mXferomodeImg = findViewById(R.id.bitmap_xfermode_image);
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.item_topics_listview_bcg_more);
         mShaderImg.setImageBitmap(roundBitmapByShader(bitmap,
                 (int) getResources().getDimension(R.dimen.round_image_width),
@@ -53,6 +57,13 @@ public class RoundImageActivity extends BaseActivity {
                 (int) getResources().getDimension(R.dimen.radius)
                 )
         );
+        mXferomodeImg.setImageBitmap(roundBitmapByXfermode(bitmap,
+                (int) getResources().getDimension(R.dimen.round_image_width),
+                (int) getResources().getDimension(R.dimen.round_image_height),
+                (int) getResources().getDimension(R.dimen.radius)
+                )
+        );
+
     }
 
     private Bitmap roundBitmapByShader(Bitmap bitmap, int outWidth, int outHeight, int radius) {
@@ -71,12 +82,45 @@ public class RoundImageActivity extends BaseActivity {
         //初始化目标bitmap
         Bitmap targetBitmap = Bitmap.createBitmap(outWidth, outHeight, Bitmap.Config.ARGB_8888);
 
+        //初始化画布
         Canvas canvas = new Canvas(targetBitmap);
 
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setShader(bitmapShader);
-
+        //全是圆角
         canvas.drawRoundRect(new RectF(0, 0, outWidth, outHeight), radius, radius, paint);
+
+        //仅仅底部是圆角
+        // 利用画笔绘制底部圆角
+//        canvas.drawRoundRect(new RectF(0, outHeight - 2 * radius, outWidth, outHeight), radius, radius, paint);
+//
+//        // 利用画笔绘制顶部上面直角部分
+//        canvas.drawRect(new RectF(0, 0, outWidth, outHeight - radius), paint);
+        return targetBitmap;
+    }
+
+    private Bitmap roundBitmapByXfermode(Bitmap bitmap, int outWidth, int outHeight, int radius){
+        if (bitmap == null)
+            throw new NullPointerException("Bitmap can not be null");
+        //初始化缩放比
+        float widthScale = outWidth * 1.0f / bitmap.getWidth();
+        float heightScale = outHeight * 1.0f / bitmap.getHeight();
+        Matrix matrix = new Matrix();
+        matrix.setScale(widthScale, heightScale);
+        Bitmap srcBitmap = Bitmap.createBitmap(bitmap, 0 , 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+        //绘制圆角图片
+        Bitmap targetBitmap = Bitmap.createBitmap(outWidth, outHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(targetBitmap);
+        canvas.drawARGB(0, 0, 0, 0);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        RectF rectF = new RectF(0, 0 , outWidth, outHeight);
+        canvas.drawRoundRect(rectF, radius, radius, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        //在画布上绘制原图
+        Rect ret = new Rect(0, 0, outWidth, outHeight);
+        canvas.drawBitmap(srcBitmap, ret, ret, paint);
         return targetBitmap;
     }
 
